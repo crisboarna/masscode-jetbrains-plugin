41a7b8511aaa359cf77875d63b09ebd7a7b011da
GitHub Action <action@github.com>
2022-07-29 08:52:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/crisboarna/masscodejetbrainsplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/crisboarna/masscodejetbrainsplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.crisboarna.masscodejetbrainsplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.crisboarna.masscodejetbrainsplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/crisboarna/masscodejetbrainsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.crisboarna.masscodejetbrainsplugin.services
+
+import com.github.crisboarna.masscodejetbrainsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
